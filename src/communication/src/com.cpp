28ed69c0cf7f5dcb28f1413308a282b677886ddf
fanyuchenzaizai <2994114386@qq.com>
2025-09-27 09:42:58 +0800

    update com && robots_msgs -> robot_msgs

---
+++ src/communication/src/com.cpp
@@ -0,0 +1,195 @@
+#include "com.hpp"
+
+namespace ns_com
+{
+
+  // 静态成员变量定义
+  MyUtils::Net::FdManager Communication::fd_manager;
+  MyUtils::MyTimer::TimerManager Communication::timer_manager;
+  std::atomic<uint16_t> Communication::arm_seq_num{0};
+  uint16_t Communication::puncture_seq_num = 0;
+  std::string Communication::STM32_PORT = "/dev/ttyACM0";
+
+  // 初始化通信模块
+  void Communication::init()
+  {
+    // 定时尝试打开串口
+    timer_manager.addTimer(1000, true, []() { Communication::__open(STM32_NAME, STM32_PORT, stm32_read_cb, 115200); });
+
+    // 启动fd_manager线程
+    std::thread([]() { fd_manager.run(); }).detach();
+
+    // 启动timer_manager线程
+    std::thread([]() { timer_manager.run(); }).detach();
+  }
+  // 打开串口
+  void Communication::__open(const std::string& name,
+                             const std::string& port,
+                             MyUtils::Net::fd_read_cb read_cb,
+                             int baud_rate,
+                             int n_bits,
+                             int stop_length,
+                             char check_type)
+  {
+    if(fd_manager.exist(name))
+      return;
+    int fd = MyUtils::Net::SerialPort::open(port, baud_rate, n_bits, stop_length, check_type);
+    if(fd != -1)
+    {
+      try
+      {
+        fd_manager.add(name, fd, read_cb);
+        std::cout << "[COM] Added " << name << " fd=" << fd << std::endl;
+      }
+      catch(const char* e)
+      {
+        std::cerr << "[COM] Error adding " << name << ": " << e << std::endl;
+      }
+    }
+    else
+    {
+      std::cerr << "[COM] Failed to open serial port for " << name << std::endl;
+    }
+  }
+  // 发送底盘目标数据包到STM32,依靠两个函数
+  // send2stm32 与 __send2stm32
+  // 构造数据包包头
+  int Communication::send2stm32(const _ChassisTarget& data_packet)
+  {
+    // 设置数据包头
+    PacketHeader header(ENUM_PACKET_ARMOR_DATA, sizeof(PacketHeader) + sizeof(_ChassisTarget));
+    header.packet_type = static_cast<uint8_t>(ENUM_PACKET_NAV_DATA);
+    //
+    header.from = static_cast<uint8_t>(ArmEnum::ENUM_ARM_SENTRY);
+    header.setTo(ArmEnum::ENUM_ARM_SLAVE_COMPUTER);
+    header.setDataLen(sizeof(data_packet));
+    return __send2stm32(header, &data_packet);
+  }
+  // 计算
+  int Communication::__send2stm32(PacketHeader& header, const void* data)
+  {
+    header.checksum = 0;
+    char* header_p = reinterpret_cast<char*>(&header);
+    for(size_t i = 0; i < sizeof(PacketHeader) / 2 - 1; ++i)
+    {
+      header.checksum += *(uint16_t*)(header_p + 2 * i);
+    }
+    header.checksum += calChecksum((char*)data, header.data_len);
+
+    MyUtils::DataType::ByteArray arr(&header, sizeof(PacketHeader));
+    arr.append(data, header.data_len);
+
+    return fd_manager.send(STM32_NAME, (char*)arr.get(), arr.size());
+  }
+
+  // STM32串口数据读取回调
+  void Communication::stm32_read_cb(ByteArray arr)
+  {
+    static ByteArray stm32_recv_buffer;
+    stm32_recv_buffer.append(arr.get(), arr.size());
+
+    while(true)
+    {
+      // 错误处理
+      if(stm32_recv_buffer.size() < 2)
+        return;
+
+      char* buf = (char*)stm32_recv_buffer.get();
+      int start_idx = -1;
+      for(size_t i = 0; i + 1 < stm32_recv_buffer.size(); ++i)
+      {
+        if((uint8_t)buf[i] == 0xA5 && (uint8_t)buf[i + 1] == 0x5A)
+        {
+          start_idx = i;
+          break;
+        }
+      }
+      if(start_idx == -1)
+      {
+        fmt::print(stderr, "[COM] Warning: Start flag(0xA5 0x5A) not found\n");
+        stm32_recv_buffer.reset();
+        return;
+      }
+      if(start_idx > 0)
+      {
+        stm32_recv_buffer = stm32_recv_buffer.sub(start_idx, stm32_recv_buffer.size() - start_idx);
+        buf = (char*)stm32_recv_buffer.get();
+      }
+      if(stm32_recv_buffer.size() < sizeof(PacketHeader))
+        return;
+
+      PacketHeader* header = (PacketHeader*)buf;
+      int full_len = sizeof(PacketHeader) + header->data_len;
+      if(stm32_recv_buffer.size() < full_len)
+        return;
+      if(check(buf, full_len))
+      {
+        switch(header->packet_type)
+        {
+        // 校验通过
+        case ENUM_PACKET_NAV_DATA:
+        {
+          const NavRes* nav_data = (const NavRes*)(buf + sizeof(PacketHeader));
+          nav_publish(nav_data);
+          break;
+        }
+        case ENUM_PACKET_UNDEFINED:
+        {
+          const RefereeInfo* referee_info = (const RefereeInfo*)(buf + sizeof(PacketHeader));
+          referee_publish(referee_info);
+          break;
+        }
+        default:
+          fmt::print(stderr, "[COM] Warning: Undefined packet type {}\n", header->packet_type);
+        }
+      }
+      else
+      {
+        fmt::print(stderr, "[COM] Warning: Checksum incorrect\n");
+      }
+      stm32_recv_buffer = stm32_recv_buffer.sub(full_len);
+    }
+  }
+
+  // 接收消息回调
+  // 导航数据发布
+  void Communication::nav_publish(const NavRes* msg)
+  {
+    static std::shared_ptr<rclcpp::Node> node;
+    static std::shared_ptr<rclcpp::Publisher<robot_msgs::msg::Nav>> pub;
+    static std::once_flag flag;
+    std::call_once(flag,
+                   []()
+                   {
+                     node = rclcpp::Node::make_shared("nav_publisher_node");
+                     pub = node->create_publisher<robot_msgs::msg::Nav>("/NavRequest", 10);
+                   });
+
+    robot_msgs::msg::Nav nav_data;
+    nav_data.target_x = msg->x;
+    nav_data.target_y = msg->y;
+    nav_data.nav_mode = robot_msgs::msg::Nav::MODE_SINGLE_POINT;
+    nav_data.header.stamp = rclcpp::Clock().now();
+    pub->publish(nav_data);
+  }
+
+  void Communication::referee_publish(const RefereeInfo* msg)
+  {
+    static std::shared_ptr<rclcpp::Node> node;
+    static std::shared_ptr<rclcpp::Publisher<robot_msgs::msg::Referee>> pub;
+    static std::once_flag flag;
+    std::call_once(flag,
+                   []()
+                   {
+                     node = rclcpp::Node::make_shared("referee_publisher_node");
+                     pub = node->create_publisher<robot_msgs::msg::Referee>("/RefereeInfo", 10);
+                   });
+
+    robot_msgs::msg::Referee referee_data;
+    referee_data.header.stamp = rclcpp::Clock().now();
+    // TO DO
+    (void)msg;
+    pub->publish(referee_data);
+  }
+
+}  // namespace ns_com
---
+++ src/communication/src/com_handle.cpp
@@ -0,0 +1,58 @@
+#include "com_handle.hpp"
+namespace ns_com
+{
+  BehavierTreeCom::BehavierTreeCom(const std::string& name)
+    : Node(name)
+  {
+    Init();
+    com_.init();
+  }
+  void BehavierTreeCom::Init()
+  {
+    cmd_vel_.linear.x = 0.0;
+    cmd_vel_.linear.y = 0.0;
+
+    chassis_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "/cmd_vel", 1, [this](const geometry_msgs::msg::Twist::ConstSharedPtr& msg) { this->sendChassisCtrlCB(msg); });
+    odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "/aft_mapped_to_init", 1, [this](const nav_msgs::msg::Odometry::ConstSharedPtr& msg) { this->odomCB(msg); });
+    gimbal_yaw_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+        "/gimbal_yaw", 1, [this](const std_msgs::msg::Float32::ConstSharedPtr& msg) { this->desiredYawCB(msg); });
+
+    RCLCPP_INFO(this->get_logger(), "\nGot /cmd_vel message");
+  }
+
+  void BehavierTreeCom::sendChassisCtrlCB(const geometry_msgs::msg::Twist::ConstSharedPtr& velPtr)
+  {
+    // 速度
+    cmd_vel_ = *velPtr;
+
+    float vx_mps = cmd_vel_.linear.x;
+    float vy_mps = cmd_vel_.linear.y;
+    float vw_rpm = 3.14;
+    // 计算雷达 yaw
+    // 计算当前 yaw 发给 current_yaw
+    tf2::Quaternion q;
+    tf2::fromMsg(odom_.pose.pose.orientation, q);
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
+    auto current_yaw = static_cast<float>(yaw * 180.0 / M_PI);
+    // 构造数据包
+    // ChassisTarget target(1, 2, 3, 4, 5, 6);
+    ChassisTarget target(vx_mps, vy_mps, vw_rpm, current_yaw, gimbal_yaw_.data, odom_.pose.pose.position.y);
+    // 发送数据包
+    com_.send2stm32(target);
+    RCLCPP_INFO(this->get_logger(), "success to send message to stm32!");
+  }
+
+  void BehavierTreeCom::odomCB(const nav_msgs::msg::Odometry::ConstSharedPtr& odomPtr)
+  {
+    odom_ = *odomPtr;
+  }
+
+  void BehavierTreeCom::desiredYawCB(const std_msgs::msg::Float32::ConstSharedPtr& yawPtr)
+  {
+    gimbal_yaw_ = *yawPtr;
+  }
+
+}  // namespace ns_com
