dafcfef36b31441a2cad5a950023d3381e99c19f
枫落醉红颜 <15207309998@163.com>
2025-11-20 21:20:38 +0800

    添加日志功能，更新GICP参数配置，优化代码结构

--- src/icp_relocalization/include/gicp_ros_interface.hpp
+++ src/icp_relocalization/include/gicp_ros_interface.hpp
@@ -16,6 +16,11 @@
 
 #include "color_text.hpp"
 #include "gicp_filter.hpp"
+#include "log.hpp"
+
+#define LOG_DEBUG(prefix, ...) icp_log::log_info((prefix), __VA_ARGS__)
+#define LOG_DEBUG_BLOCK(prefix, ...) icp_log::log_block((prefix), __VA_ARGS__)
+
 namespace icp_relocalization
 {
 
@@ -42,8 +47,8 @@ namespace icp_relocalization
 
     void publishPose(const Eigen::Matrix4f& pose, const rclcpp::Time& stamp);
 
+    // ROS 接口
     std::unique_ptr<GicpFilter> gicp_filter_;
-
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
@@ -51,35 +56,36 @@ namespace icp_relocalization
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-
+    rclcpp::CallbackGroup::SharedPtr callback_group_lidar_;
+    rclcpp::CallbackGroup::SharedPtr callback_group_odom_;
     rclcpp::TimerBase::SharedPtr fsm_timer_;
     void fsmTimerCallback();
     void runFSM();
 
+    // 状态与缓存
+    State state_ = State::UNINITIALIZED;
+    std::deque<nav_msgs::msg::Odometry::SharedPtr> odom_buffer_;
+    nav_msgs::msg::Odometry::SharedPtr last_odom_;
     PointCloud::Ptr latest_cloud_;
     std::string latest_cloud_frame_;
     rclcpp::Time latest_cloud_stamp_;
     bool has_new_cloud_ = false;
-
-    rclcpp::CallbackGroup::SharedPtr callback_group_lidar_;
-    rclcpp::CallbackGroup::SharedPtr callback_group_odom_;
-
-    State state_ = State::UNINITIALIZED;
-    std::deque<nav_msgs::msg::Odometry::SharedPtr> odom_buffer_;
-    nav_msgs::msg::Odometry::SharedPtr last_odom_;
-    Eigen::Matrix4f last_icp_pose_ = Eigen::Matrix4f::Identity();
-    Eigen::Matrix4f map_to_camera_init_ = Eigen::Matrix4f::Identity();
     bool gicp_initialized_ = false;
     rclcpp::Time last_icp_time_;
 
+    // 变换与位姿
+    Eigen::Matrix4f map_to_camera_init_ = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f last_icp_pose_ = Eigen::Matrix4f::Identity();  // base_link 在 map 下的最新融合位姿
+
+    // 默认参数
     std::string base_frame_;
     std::string map_frame_;
-    double drift_threshold_m_;    // IMU与ICP位置差异阈值
-    double drift_threshold_rad_;  // IMU与ICP姿态差异阈值
-    double icp_frequency_;        // ICP执行频率
-    bool use_initial_alignment_;  // 是否启用SAC-IA进行初始定位
+    double drift_threshold_m_;    // 位置漂移阈值
+    double drift_threshold_rad_;  // 姿态漂移阈值
+    double alignment_frequency_;  // 地图对齐(GICP)低频执行频率
+    bool publish_pose_on_odom_;   // 是否在每次里程计回调发布融合位姿
+    bool use_initial_alignment_;  // 是否启用 SAC-IA 初始定位
     GicpFilter::Options gicp_options_;
-
   };
 
 }  // namespace icp_relocalization
