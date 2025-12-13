60574f9784b74ffcfdc6e0eaca1d5380e72f8e16
枫落醉红颜 <15207309998@163.com>
2025-12-13 09:44:29 +0800

    add analyzer demo, elecvation demo and gicp_relocalization refactor

--- src/navigation/navi2_bringup/launch/navigation2.launch.py
+++ src/navigation/navi2_bringup/launch/navigation2.launch.py
@@ -20,7 +20,6 @@ def generate_launch_description():
         'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026.yaml'))
     nav2_param_path = launch.substitutions.LaunchConfiguration(
         'params_file', default=os.path.join(DreamChaser_dir, 'params', 'sentry2.yaml'))
-        # 'params_file', default=os.path.join(DreamChaser_dir, 'params', 'nav2_params_revise.yaml'))
 
     return launch.LaunchDescription([
         # 静态TF: map -> camera_init
@@ -31,7 +30,7 @@ def generate_launch_description():
             arguments=['0', '0', '0', '0', '0', '0', 'map', 'camera_init'],
             output='screen'),
 
-        # # 静态TF: camera_init -> body
+        # 静态TF: camera_init -> body
         # launch_ros.actions.Node(
         #     package='tf2_ros',
         #     executable='static_transform_publisher',
@@ -62,7 +61,7 @@ def generate_launch_description():
                 'map': map_yaml_path,
                 'use_sim_time': use_sim_time,
                 'params_file': nav2_param_path}.items(),
-        ),
+        ),           
         launch_ros.actions.Node(
             package='rviz2',
             executable='rviz2',
