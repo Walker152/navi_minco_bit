6eebbb234ca1627edb9493e8b28516c5ed7a1e24
alioth <15207309998@163.com>
2025-12-20 21:56:11 +0800

    能跑

--- src/decision/bt_manager/src/bt_manager.cpp
+++ src/decision/bt_manager/src/bt_manager.cpp
@@ -19,6 +19,12 @@ namespace Sentry_BT
     factory_.registerNodeType<WaitUntilStopped>("WaitUntilStopped");
     factory_.registerNodeType<Wait>("Wait");
 
+    factory_.registerNodeType<CheckMPCondition>("CheckMPCondition");
+    factory_.registerNodeType<CheckAPCondition>("CheckAPCondition");
+    factory_.registerNodeType<CheckDPCondition>("CheckDPCondition");
+    factory_.registerNodeType<CheckWhetherChange>("CheckWhetherChange");
+    factory_.registerNodeType<ChangePosition>("ChangePosition");
+    factory_.registerNodeType<ChangePosition>("JustProtect");
     // 创建行为树
     try
     {
