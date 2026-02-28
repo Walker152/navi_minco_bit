6040cae83e55fcc25cd53b4b3db98e8fcc6cf6ac
alioth <15207309998@163.com>
2026-02-28 19:33:17 +0800

    little revise

--- src/perception/icp_relocalization/src/gicp_ros_interface.cpp
+++ src/perception/icp_relocalization/src/gicp_ros_interface.cpp
@@ -119,7 +119,7 @@ namespace icp_relocalization
     lidar_sub_options.callback_group = callback_group_lidar_;
 
     lidar_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        "/livox/stdpc",
+        "/cloud_registered",
         rclcpp::SensorDataQoS(),
         std::bind(&GicpRosInterface::lidarCallback, this, std::placeholders::_1),
         lidar_sub_options);
