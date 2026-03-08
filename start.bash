5b6c4754aa681c9d79c00edec56cc66594933918
zhong-jia-xu <wer22293@163.com>
2026-03-08 12:56:56 +0000

    fix:update communication protocal

--- start.bash
+++ start.bash
@@ -1,30 +1,26 @@
-# COM:CAN
-source ~/2025-sentry-navi/install/setup.bash             
-
-# gnome-terminal -- bash -c "ros2 launch yhs_twist_converter com.launch.py; exec bash"
-
-# # MID360
+# !/bin/bash 
+# MID360
 source ~/ws_livox/install/setup.bash
 # sleep 1s
 gnome-terminal -- bash -c "ros2 launch livox_ros_driver2 msg_MID360_launch.py ; exec bash"
 sleep 1s
-# sleep 30s
+
+source ~/2025-sentry-navi/install/setup.bash
+
 # SLAM
 gnome-terminal -- bash -c "ros2 launch point_lio point_lio.launch.py ; exec bash"
 sleep 1s
+
 # Navigation2
-source ~/2025-sentry-navi/install/setup.bash
-
-
 gnome-terminal -- bash -c "ros2 launch navi2 navigation2.launch.py; exec bash"
 sleep 4s
 
-gnome-terminal -- bash -c "ros2 launch icp_relocalization gicp_relocalization.launch.py ; exec bash"
-sleep 2s
+# gnome-terminal -- bash -c "ros2 launch icp_relocalization gicp_relocalization.launch.py ; exec bash"
+
 # BT-manager
 # gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
-sleep 1s
-#gnome-terminal -- bash -c "ros2 launch bt_manager test.launch.py; exec bash"
+
 # COM
-# gnome-terminal -- bash -c "ros2 launch communication com.launch.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch communication com.launch.py; exec bash"
+
 # gnome-terminal -- bash -c "cd /home/rm/rosbag && ros2 bag record -a ;exec bash"
