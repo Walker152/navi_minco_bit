5a993f0109ff3958cb5e0f8dc3285c23582044eb
luomu <2917433538@qq.com>
2026-03-09 14:16:32 +0800

    改了点communication的代码,没啥用

--- play.bash
+++ play.bash
@@ -8,5 +8,7 @@ sleep 0.5s
 # BT-manager
 gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
 sleep 0.5s
+# commmunication
+gnome-terminal -- bash -c "ros2 launch communication com.launch.py; exec bash"
 
 # tODESK
