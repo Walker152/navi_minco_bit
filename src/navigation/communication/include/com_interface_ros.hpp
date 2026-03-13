d51b4800c49b0d849c1b7acc8d3b5e16c8cfd5b5
alioth <15207309998@163.com>
2026-03-13 17:01:49 +0800

    importantgit add .git add .git add .! stable for all,except tracing

--- src/navigation/communication/include/com_interface_ros.hpp
+++ src/navigation/communication/include/com_interface_ros.hpp
@@ -145,7 +145,7 @@ namespace ns_com
         vx_mps = cmd_vel_.linear.x;
         vy_mps = cmd_vel_.linear.y;
         vw_rpm = static_cast<float>(cmd_vel_.angular.z * 60.0 / (2.0 * M_PI));
-        // vw_rpm = 0.0f;
+        vw_rpm = 40.0f;
         // desire_stance = ros_interfaces::msg::Behavior::STANCE_MOVE; // TODO: 这里暂时写死，后续根据实际情况修改
         // desire_lifter_pos = LifterPos::BOTTOM; // TODO: 这里暂时写死，后续根据实际情况修改
         desire_stance = behavior_.desired_stance; // 之前写死了，现作修改
