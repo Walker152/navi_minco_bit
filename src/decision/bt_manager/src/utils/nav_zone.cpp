7f2fba24610dbcad900da2bd0d1f3f76b3af5017
枫落醉红颜 <15207309998@163.com>
2026-03-30 20:48:45 +0800

    feat:map change behavior tested

--- src/decision/bt_manager/src/utils/nav_zone.cpp
+++ src/decision/bt_manager/src/utils/nav_zone.cpp
@@ -3,9 +3,9 @@
 namespace Sentry_BT
 {
   std::vector<Point2D> nav_points = {
-    //   {3.0, 3.0},  //HOME
-    //   {8.5, 8.5},  // BONUS
-    //   {15.7, 11.0}  // OUTPOST
+      {3.0, 3.0},  //HOME
+      {8.5, 8.5},  // BONUS
+      {15.7, 11.0}  // OUTPOST
 
       // for test
       // {1.8, 5.6},  //HOME
@@ -13,19 +13,19 @@ namespace Sentry_BT
       // {7.2, 6.0}   //OUTPOST
 
     // for rmul
-      {1.2, 7.2},  //HOME
-      {6.4, 4.4},  // BONUS
-      {7.5, 6.8}  // OUTPOST
+      // {1.2, 7.2},  //HOME
+      // {6.4, 4.4},  // BONUS
+      // {7.5, 6.8}  // OUTPOST
   };
 
   std::vector<Sentry_BT::Point2D> patrol_points_normal = {
     // for rmuc
-    //   {14.2, 11.5},
-    //   {17.5, 8.2},
-    //   {16.0, 4.2},
-    //   {12.4, 5.1},
-    //   {12.0, 8.6},
-    //   {14.0, 12.0}
+      {14.2, 11.5},
+      {17.5, 8.2},
+      {16.0, 4.2},
+      // {12.4, 5.1},
+      // {12.0, 8.6},
+      // {14.0, 12.0}
     // for test
       // {5.6, 6.0},
       // {4.5, 5.1},
@@ -33,23 +33,23 @@ namespace Sentry_BT
 
     // for rmul
      // {7.2, 5.3},
-      {6.4, 4.4},
+      // {6.4, 4.4},
       // {5.5, 5.3}
       // {7.3, 4.5}
   };
 
   std::vector<int> patrol_points_milliseconds = {
-      10000,
-      // 5000
-    //   6000,
+      5000,
+      5000,
+      6000,
     //   2000,
     //   2000,
   };
 
   std::vector<Sentry_BT::Point2D> patrol_points_attack = {
-    //   {15.2, 11.5},
-    //   {12.8, -0.1},
-    //   {11.5, -3.8}
+      {15.2, 11.5},
+      {12.8, -0.1},
+      {11.5, -3.8}
 
     // for test
     // {5.6, 6.0},
@@ -58,7 +58,7 @@ namespace Sentry_BT
 
     // for rmul
       // {7.2, 5.3},
-      {6.4, 4.4}, 
+      // {6.4, 4.4}, 
       // {5.5, 5.3}
       // {7.3, 4.5}
   };
