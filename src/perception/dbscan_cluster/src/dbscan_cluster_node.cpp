0a43d6842d63c8b757863064a4047c920e1f7fc7
枫落醉红颜 <15207309998@163.com>
2026-04-02 12:38:23 +0800

    feat:change eudi to dbscan

--- src/perception/dbscan_cluster/src/dbscan_cluster_node.cpp
+++ src/perception/dbscan_cluster/src/dbscan_cluster_node.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<EuclideanCluster::RosInterface>();
+    auto node = std::make_shared<DBSCANCluster::RosInterface>();
     rclcpp::spin(node);
     rclcpp::shutdown();
 
