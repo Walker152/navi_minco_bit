467eef1d22bc70bfffd61d61c9643ee0836add41
枫落醉红颜 <15207309998@163.com>
2026-04-11 21:02:18 +0800

    feat:add vision data into cluster

--- src/perception/dbscan_cluster/include/ros_interface.hpp
+++ src/perception/dbscan_cluster/include/ros_interface.hpp
@@ -2,15 +2,25 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
 #include <ros_interfaces/msg/dynamic_obstacle.hpp>
 #include <ros_interfaces/msg/dynamic_obstacle_array.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
+
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 #include "dbscan_cluster.hpp"
 #include "kalman_tracker.hpp"
 
 namespace DBSCANCluster
 {
+
+struct VisionTarget
+{
+  Eigen::Vector3f position;
+  Eigen::Vector3f velocity;
+};
 
 class RosInterface : public rclcpp::Node
 {
@@ -19,6 +29,7 @@ public:
 
 private:
   void pointCloudCallback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg);
+  void visionCallback(const geometry_msgs::msg::PoseArray::ConstSharedPtr msg);
 
   DBSCANClusterAlg cluster_alg_;
   KalmanTracker tracker_alg_;
@@ -26,11 +37,18 @@ private:
   TrackerConfig tracker_config_;
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_point_cloud_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub_vision_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr pub_markers_;
   rclcpp::Publisher<ros_interfaces::msg::DynamicObstacleArray>::SharedPtr pub_obstacles_;
 
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
   rclcpp::Time last_stamp_{0, 0, RCL_ROS_TIME};
   bool has_last_stamp_ = false;
+  
+  std::vector<VisionTarget> latest_vision_targets_;
+  std::mutex vision_mutex_;
 };
 
 }  // namespace DBSCANCluster
