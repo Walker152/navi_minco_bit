d9db730dfc86d6cc6437752c3a6625ce2a0082b8
alioth <15207309998@163.com>
2026-04-14 19:39:48 +0800

    first tested, behavior position stacked

--- src/navigation/navi2_bringup/launch/navigation2.launch.py
+++ src/navigation/navi2_bringup/launch/navigation2.launch.py
@@ -34,26 +34,27 @@ def generate_launch_description():
             name='static_tf_map_to_camera_init',
             # arguments=['1.73', '6.4', '0', '0.0', '0', '0.0', 'map', 'camera_init'],
             arguments=['5', '7', '0', '0', '0', '0', 'map', 'camera_init'],
+            # arguments=['8', '5', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['6.8', '3.67', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['12', '4.5', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['9', '6.0', '0', '0', '0', '0', 'map', 'camera_init'],
             output='screen'),
 
-        # 静态TF: camera_init -> body
-        launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf_camera_init_to_body',
-            arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
-            output='screen'),
+        # # 静态TF: camera_init -> body
+        # launch_ros.actions.Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_tf_camera_init_to_body',
+        #     arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
+        #     output='screen'),
 
-        # 静态TF: body -> base_link
-        launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf_body_to_base_link',
-            arguments=['0', '0.15', '0', '0', '0', '0', 'body', 'base_link'],
-            output='screen'),
+        # # 静态TF: body -> base_link
+        # launch_ros.actions.Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_tf_body_to_base_link',
+        #     arguments=['0', '0.15', '0', '0', '0', '0', 'body', 'base_link'],
+        #     output='screen'),
         # 声明新的 Launch 参数
         launch.actions.DeclareLaunchArgument('use_sim_time', default_value=use_sim_time,
                                              description='Use simulation (Gazebo) clock if true'),
