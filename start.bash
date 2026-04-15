814ee2292996cc74291a4a7886077f672ad04ef7
luomu <2917433538@qq.com>
2026-04-15 18:58:48 +0800

    合并

--- start.bash
+++ start.bash
@@ -2,13 +2,13 @@
 cd ~/2025-sentry-navi
 source ./install/setup.bash
 
-# # MID360 
+# MID360 
 gnome-terminal -- bash -c "ros2 launch livox_ros_driver2 msg_MID360_launch.py; exec bash"
-# sleep 3
+sleep 3
 
-# # SLAM
+# SLAM
 gnome-terminal -- bash -c "ros2 launch point_lio point_lio.launch.py; exec bash"
-# sleep 1
+sleep 1
 
 # Navigation2
 gnome-terminal -- bash -c "ros2 launch navi2 navigation2.launch.py; exec bash"
@@ -25,9 +25,13 @@ sleep 1
 
 ## Decision
 gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
-# sleep 1
+sleep 1
 
 # Communication
 gnome-terminal -- bash -c "ros2 launch communication com.launch.py; exec bash"
 
-# gnome-terminal -- bash
+# Rosbag record
+# gnome-terminal -- bash -c "mkdir -p ~/rosbag && ros2 bag record -a -o ~/rosbag/$(date +%Y%m%d_%H%M%S); exec bash"
+
+# Foxglove
+# gnome-terminal -- bash -c "ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765; exec bash"
