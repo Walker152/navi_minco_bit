b6442b19286595d40c679d70c9a1f7ddc30acdcb
枫落醉红颜 <15207309998@163.com>
2026-04-17 20:20:54 +0800

    feat:new map,mpc pitch compensetion

--- src/utils/pcd_trans/pcd_tool.py
+++ src/utils/pcd_trans/pcd_tool.py
@@ -199,7 +199,7 @@ def main():
     OUTPUT_PCD = "src/navigation/navi2_bringup/maps/pcd/2026rmuc_trans.pcd"
     
     # 平移参数 (单位: 米)
-    TX = -0.15
+    TX = -0.30
     TY = -0.6
     TZ = 0.0
     
