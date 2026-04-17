46b6ebac22ee8d98d42f54f2ca86f816cd7be6f4
枫落醉红颜 <15207309998@163.com>
2026-04-17 20:20:54 +0800

    feat:new map,mpc pitch compensetion

--- src/navigation/navi2_bringup/launch/navigation2.launch.py
+++ src/navigation/navi2_bringup/launch/navigation2.launch.py
@@ -18,11 +18,11 @@ def generate_launch_description():
         'use_sim_time', default='False')
     
     map_yaml_path = launch.substitutions.LaunchConfiguration(
-        # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026.yaml'))
+        'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026.yaml'))
         # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmul2026.yaml'))
         # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026_back.yaml'))
         #  'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/room.yaml'))
-        'map', default=os.path.join(DreamChaser_dir, 'maps', 'first_floor/first_floor.yaml'))
+        # 'map', default=os.path.join(DreamChaser_dir, 'maps', 'first_floor/first_floor.yaml'))
     nav2_param_path = launch.substitutions.LaunchConfiguration(
         'params_file', default=os.path.join(DreamChaser_dir, 'params', 'sentry1.yaml'))
 
@@ -33,30 +33,30 @@ def generate_launch_description():
             executable='static_transform_publisher',
             name='static_tf_map_to_camera_init',
             # arguments=['1.73', '6.4', '0', '0.0', '0', '0.0', 'map', 'camera_init'],
-            # arguments=['5', '7', '0', '0', '0', '0', 'map', 'camera_init'],
+            arguments=['5', '7', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['8', '5', '0', '0', '0', '0', 'map', 'camera_init'],
-            arguments=['10.8', '4.07', '0', '0', '0', '0', 'map', 'camera_init'],
+            # arguments=['10.8', '4.07', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['10', '4.5', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['9', '6.0', '0', '0', '0', '0', 'map', 'camera_init'],
             output='screen'),
 
-        # 静态TF: map -> minimap
-        launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf_map_to_minimap',
-            # 红方使用全0，蓝方使用28,12
-            # arguments=['0', '0', '0', '0', '0', '0', 'map', 'minimap'],
-            arguments=['28', '15', '0', '3.14', '0', '0', 'map', 'minimap'],
-            output='screen'),
+        # # 静态TF: map -> minimap
+        # launch_ros.actions.Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_tf_map_to_minimap',
+        #     # 红方使用全0，蓝方使用28,12
+        #     # arguments=['0', '0', '0', '0', '0', '0', 'map', 'minimap'],
+        #     arguments=['28', '15', '0', '3.14', '0', '0', 'map', 'minimap'],
+        #     output='screen'),
 
-        # 静态TF: camera_init -> body
-        launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf_camera_init_to_body',
-            arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
-            output='screen'),
+        # # 静态TF: camera_init -> body
+        # launch_ros.actions.Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_tf_camera_init_to_body',
+        #     arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
+        #     output='screen'),
 
         # 静态TF: body -> base_link
         launch_ros.actions.Node(
