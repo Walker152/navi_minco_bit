e3a39ec3a8dbb218a79096feebd3dfc072e3e9d4
枫落醉红颜 <15207309998@163.com>
2026-04-23 13:07:00 +0800

    fix:revise stance logic

--- start.bash
+++ start.bash
@@ -15,7 +15,7 @@ gnome-terminal -- bash -c "ros2 launch navi2 navigation2.launch.py; exec bash"
 sleep 5
 
 ## ICP
-gnome-terminal -- bash -c "ros2 launch icp_relocalization gicp_relocalization.launch.py; exec bash"
+# gnome-terminal -- bash -c "ros2 launch icp_relocalization gicp_relocalization.launch.py; exec bash"
 # sleep 1
 
 
