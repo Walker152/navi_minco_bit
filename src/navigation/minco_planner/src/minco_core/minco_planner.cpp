15d54bb05e13b171c9769d1f2139baa42bf6c621
alioth <15207309998@163.com>
2026-04-24 22:12:34 +0800

    tested

--- src/navigation/minco_planner/src/minco_core/minco_planner.cpp
+++ src/navigation/minco_planner/src/minco_core/minco_planner.cpp
@@ -1111,8 +1111,8 @@ void MincoPlanner::prepareColdStart(const geometry_msgs::msg::Pose & start_pose,
         local_dir /= norm;
         constexpr double min_climb_speed = 1.5;
         if (std::hypot(real_speed.x(), real_speed.y()) < min_climb_speed) {
-          real_speed.x() = local_dir.x() * min_climb_speed;
-          real_speed.y() = local_dir.y() * min_climb_speed;
+          real_speed.x() += local_dir.x() * min_climb_speed;
+          real_speed.y() += local_dir.y() * min_climb_speed;
         }
       }
     }
