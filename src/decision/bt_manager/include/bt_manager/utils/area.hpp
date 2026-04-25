30b014f5868b4a10c318ec8a43d944c256225208
alioth <15207309998@163.com>
2026-04-25 16:25:30 +0800

    4.25 test

--- src/decision/bt_manager/include/bt_manager/utils/area.hpp
+++ src/decision/bt_manager/include/bt_manager/utils/area.hpp
@@ -1,5 +1,6 @@
 #pragma once
-
+// #define RMUC_AREA
+#define TEST_AREA
 #include "bt_manager/utils/nav_zone.hpp"
 
 #include <array>
@@ -8,143 +9,145 @@
 #include <vector>
 // clang-format off
 namespace Sentry_BT {
+#ifdef RMUC_AREA
 // for rmuc
-// inline std::array<Area_Square, 4> transform_zone{
-//   Area_Square{Point2D{10.5, 5.0}, Point2D{8.0, 1.3}},
-//   Area_Square{Point2D{21.0, 13.2}, Point2D{18.5, 10.0}},
-//   Area_Square{Point2D{15.7, 13.8}, Point2D{10.4, 12.3}},
-//   Area_Square{Point2D{18.6, 2.7}, Point2D{13.3, 1.2}},
-// };
-// inline std::array<Area_Square, 2> bonus_zone = {
-//   Area_Square{Point2D{12.8, 5.5}, Point2D{13.8, 6.5}},
-//   Area_Square{Point2D{14.7, 11.0}, Point2D{15.7, 12.0}},
-// };  // 假设这是奖励区域的坐标范围
-// inline std::array<Area_Square, 4> tunnel_zone = {
-//   Area_Square{Point2D{10.4, 3.6}, Point2D{9.3, 1.8}},
-//   Area_Square{Point2D{14.1, 13.8}, Point2D{12.1, 13.0}},
-//   Area_Square{Point2D{19.7, 13.2}, Point2D{18.6, 11.4}},
-//   Area_Square{Point2D{16.9, 2.0}, Point2D{14.9, 1.2}},
-// };
-// inline std::array<Area_Square, 2> stairs_zone{
-//   Area_Square{Point2D{9.4, 1.8}, Point2D{8.0, 0.2}},
-//   Area_Square{Point2D{19.6, 13.2}, Point2D{21.0, 14.8}},
-// };  // 假设这是楼梯区域的坐标范围
-// inline std::array<Area_Square, 2> stairs_lower_safe_zone{
-//   Area_Square{Point2D{9.4, 3.4}, Point2D{8.1, 2.2}},
-//   Area_Square{Point2D{19.6, 11.8}, Point2D{20.9, 12.8}},
-// };
-// inline AreaPolygon<8, Point2D> highland_zone{
-//   Point2D{13.2, 12.7}, 
-//   Point2D{10.9, 9.5},
-//   Point2D{10.9, 6.5},
-//   Point2D{13.5, 2.3},
-//   Point2D{15.8, 2.3},
-//   Point2D{18.1, 5.5},
-//   Point2D{18.1, 8.5},
-//   Point2D{15.5, 12.7},
-// };
-// inline AreaPolygon<8, Point2D> own_defense_zone{
-//   Point2D{0.3, 9.8}, 
-//   Point2D{8.3, 9.8},
-//   Point2D{11.0, 13.6},
-//   Point2D{12.9, 13.6},
-//   Point2D{9.6, 8.4},
-//   Point2D{9.6, 6.9},
-//   Point2D{10.6, 4.4},
-//   Point2D{0.3, 4.4},
-// };
-// inline AreaPolygon<8, Point2D> enemy_defense_zone{
-//   Point2D{28.7, 5.2}, 
-//   Point2D{20.7, 5.2},
-//   Point2D{18.0, 1.4},
-//   Point2D{16.1, 1.4},
-//   Point2D{19.4, 6.6},
-//   Point2D{19.4, 8.1},
-//   Point2D{18.4, 10.6},
-//   Point2D{28.7, 10.6},
-// };
-// inline Area_Square enemy_outpost_watch_zone{Point2D{16.7, 12.5}, Point2D{13.5, 10.1}};
+inline std::array<Area_Square, 4> transform_zone{
+  Area_Square{Point2D{10.5, 5.0}, Point2D{8.0, 1.3}},
+  Area_Square{Point2D{21.0, 13.2}, Point2D{18.5, 10.0}},
+  Area_Square{Point2D{15.7, 13.8}, Point2D{10.4, 12.3}},
+  Area_Square{Point2D{18.6, 2.7}, Point2D{13.3, 1.2}},
+};
+inline std::array<Area_Square, 2> bonus_zone = {
+  Area_Square{Point2D{12.8, 5.5}, Point2D{13.8, 6.5}},
+  Area_Square{Point2D{14.7, 11.0}, Point2D{15.7, 12.0}},
+};  // 假设这是奖励区域的坐标范围
+inline std::array<Area_Square, 4> tunnel_zone = {
+  Area_Square{Point2D{10.4, 3.6}, Point2D{9.3, 1.8}},
+  Area_Square{Point2D{14.1, 13.8}, Point2D{12.1, 13.0}},
+  Area_Square{Point2D{19.7, 13.2}, Point2D{18.6, 11.4}},
+  Area_Square{Point2D{16.9, 2.0}, Point2D{14.9, 1.2}},
+};
+inline std::array<Area_Square, 2> stairs_zone{
+  Area_Square{Point2D{9.4, 1.8}, Point2D{8.0, 0.2}},
+  Area_Square{Point2D{19.6, 13.2}, Point2D{21.0, 14.8}},
+};  // 假设这是楼梯区域的坐标范围
+inline std::array<Area_Square, 2> stairs_lower_safe_zone{
+  Area_Square{Point2D{9.4, 3.4}, Point2D{8.1, 2.2}},
+  Area_Square{Point2D{19.6, 11.8}, Point2D{20.9, 12.8}},
+};
+inline AreaPolygon<8, Point2D> highland_zone{
+  Point2D{13.2, 12.7}, 
+  Point2D{10.9, 9.5},
+  Point2D{10.9, 6.5},
+  Point2D{13.5, 2.3},
+  Point2D{15.8, 2.3},
+  Point2D{18.1, 5.5},
+  Point2D{18.1, 8.5},
+  Point2D{15.5, 12.7},
+};
+inline AreaPolygon<8, Point2D> own_defense_zone{
+  Point2D{0.3, 9.8}, 
+  Point2D{8.3, 9.8},
+  Point2D{11.0, 13.6},
+  Point2D{12.9, 13.6},
+  Point2D{9.6, 8.4},
+  Point2D{9.6, 6.9},
+  Point2D{10.6, 4.4},
+  Point2D{0.3, 4.4},
+};
+inline AreaPolygon<8, Point2D> enemy_defense_zone{
+  Point2D{28.7, 5.2}, 
+  Point2D{20.7, 5.2},
+  Point2D{18.0, 1.4},
+  Point2D{16.1, 1.4},
+  Point2D{19.4, 6.6},
+  Point2D{19.4, 8.1},
+  Point2D{18.4, 10.6},
+  Point2D{28.7, 10.6},
+};
+inline Area_Square enemy_outpost_watch_zone{Point2D{16.7, 12.5}, Point2D{13.5, 10.1}};
 
-// inline AreaPolygon<6, Point2D> own_highland_buff_zone{
-//   Point2D{13.0, 12.3},
-//   Point2D{13.0, 11.1},
-//   Point2D{11.8, 9.5},
-//   Point2D{11.8, 6.5},
-//   Point2D{11.0, 6.5},
-//   Point2D{11.0, 9.5},
-// };
-// inline AreaPolygon<6, Point2D> enemy_highland_buff_zone{
-//   Point2D{16.0, 2.7},
-//   Point2D{16.0, 3.9},
-//   Point2D{17.2, 5.5},
-//   Point2D{17.2, 8.5},
-//   Point2D{18.0, 8.5},
-//   Point2D{18.0, 5.5},
-// };
+inline AreaPolygon<6, Point2D> own_highland_buff_zone{
+  Point2D{13.0, 12.3},
+  Point2D{13.0, 11.1},
+  Point2D{11.8, 9.5},
+  Point2D{11.8, 6.5},
+  Point2D{11.0, 6.5},
+  Point2D{11.0, 9.5},
+};
+inline AreaPolygon<6, Point2D> enemy_highland_buff_zone{
+  Point2D{16.0, 2.7},
+  Point2D{16.0, 3.9},
+  Point2D{17.2, 5.5},
+  Point2D{17.2, 8.5},
+  Point2D{18.0, 8.5},
+  Point2D{18.0, 5.5},
+};
 
-// inline AreaPolygon<6, Point2D> own_base_buff_zone{
-//   Point2D{2.9, 9.4}, 
-//   Point2D{4.4, 8.5},
-//   Point2D{4.4, 6.5},
-//   Point2D{2.9, 5.6},
-//   Point2D{1.6, 6.4},
-//   Point2D{1.6, 8.8},
-// };
-// inline AreaPolygon<6, Point2D> enemy_base_buff_zone{
-//   Point2D{26.1, 5.6},
-//   Point2D{24.6, 6.5},
-//   Point2D{24.6, 8.5},
-//   Point2D{26.1, 9.4},
-//   Point2D{27.4, 8.6},
-//   Point2D{27.4, 6.2},
-// };
-// inline AreaPolygon<6, Point2D> own_outpost_buff_zone{
-//   Point2D{10.9, 2.5},
-//   Point2D{11.7, 2.5},
-//   Point2D{12.2, 3.0},
-//   Point2D{12.2, 4.0},
-//   Point2D{11.7, 4.5},
-//   Point2D{10.9, 4.5},
-// };
-// inline AreaPolygon<6, Point2D> enemy_outpost_buff_zone{
-//   Point2D{18.1, 12.5},
-//   Point2D{17.3, 12.5},
-//   Point2D{16.8, 12.0},
-//   Point2D{16.8, 11.0},
-//   Point2D{17.3, 10.5},
-//   Point2D{18.1, 10.5},
-// };
+inline AreaPolygon<6, Point2D> own_base_buff_zone{
+  Point2D{2.9, 9.4}, 
+  Point2D{4.4, 8.5},
+  Point2D{4.4, 6.5},
+  Point2D{2.9, 5.6},
+  Point2D{1.6, 6.4},
+  Point2D{1.6, 8.8},
+};
+inline AreaPolygon<6, Point2D> enemy_base_buff_zone{
+  Point2D{26.1, 5.6},
+  Point2D{24.6, 6.5},
+  Point2D{24.6, 8.5},
+  Point2D{26.1, 9.4},
+  Point2D{27.4, 8.6},
+  Point2D{27.4, 6.2},
+};
+inline AreaPolygon<6, Point2D> own_outpost_buff_zone{
+  Point2D{10.9, 2.5},
+  Point2D{11.7, 2.5},
+  Point2D{12.2, 3.0},
+  Point2D{12.2, 4.0},
+  Point2D{11.7, 4.5},
+  Point2D{10.9, 4.5},
+};
+inline AreaPolygon<6, Point2D> enemy_outpost_buff_zone{
+  Point2D{18.1, 12.5},
+  Point2D{17.3, 12.5},
+  Point2D{16.8, 12.0},
+  Point2D{16.8, 11.0},
+  Point2D{17.3, 10.5},
+  Point2D{18.1, 10.5},
+};
 
-// inline std::vector<Point2D> nav_points = {
-//   {3.0, 3.0, 0.0},   // HOME
-//   {12.8, 5.5, 0.0},   // BONUS
-//   {15.7, 11.0, 0.0}, // OUTPOST
-//   {7.2, 7.5, 0.0},   // OWN_FORT
-//   {22.0, 7.5, 0.0}   // ENEMY_FORT
+inline std::vector<Point2D> nav_points = {
+  {3.0, 3.0, 0.0},   // HOME
+  {12.8, 5.5, 0.0},   // BONUS
+  {15.7, 11.0, 0.0}, // OUTPOST
+  {7.2, 7.5, 0.0},   // OWN_FORT
+  {22.0, 7.5, 0.0}   // ENEMY_FORT
 
-//   // for rmul
-//   // {1.2, 7.2, 0.0},  //HOME
-//   // {6.4, 4.4, 0.0},  // BONUS
-//   // {7.5, 6.8, 0.0}  // OUTPOST
-// };
+  // for rmul
+  // {1.2, 7.2, 0.0},  //HOME
+  // {6.4, 4.4, 0.0},  // BONUS
+  // {7.5, 6.8, 0.0}  // OUTPOST
+};
 
-// inline std::vector<PatrolPoint> patrol_points_normal = {
-//   // for rmuc
-//   {{16.0, 12.0, 0.0}, 5000},
-//   {{17.3, 7.9, 0.0}, 5000},
-//   {{15.3, 3.8, 0.0}, 6000},
-// };
+inline std::vector<PatrolPoint> patrol_points_normal = {
+  // for rmuc
+  {{16.0, 12.0, 0.0}, 5000},
+  {{17.3, 7.9, 0.0}, 5000},
+  {{15.3, 3.8, 0.0}, 6000},
+};
 
-// inline std::vector<PatrolPoint> patrol_points_attack = {
-//   {{16.0, 12.0, 0.0}, 5000}, {{17.3, 7.9, 0.0}, 5000}, {{15.3, 3.8, 0.0}, 6000}
-// };
-
+inline std::vector<PatrolPoint> patrol_points_attack = {
+  {{16.0, 12.0, 0.0}, 5000}, {{17.3, 7.9, 0.0}, 5000}, {{15.3, 3.8, 0.0}, 6000}
+};
+#endif
+#ifdef TEST_AREA
 // for test
 inline std::array<Area_Square, 4> transform_zone{
-  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 3.1}},
-  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 3.1}},
-  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 3.1}},
-  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 3.1}},
+  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 2.1}},
+  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 2.1}},
+  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 2.1}},
+  Area_Square{Point2D{12.6, 7.3}, Point2D{9.6, 2.1}},
 };
 inline std::array<Area_Square, 2> bonus_zone = {
   Area_Square{Point2D{12.8, 5.5}, Point2D{13.8, 6.5}},
@@ -266,6 +269,7 @@ inline std::vector<PatrolPoint> patrol_points_normal = {
 inline std::vector<PatrolPoint> patrol_points_attack = {
   {{16.0, 12.0, 0.0}, 5000}, {{17.3, 7.9, 0.0}, 5000}, {{15.3, 3.8, 0.0}, 6000}
 };
+#endif
 // =============== 战略模式：巡逻点与云台巡检区域映射表 ===============
 
 using PatrolList = std::vector<PatrolPoint>;
