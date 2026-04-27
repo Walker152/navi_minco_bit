71858bd1fafb002e18f4f09381d967248f225a30
alioth <15207309998@163.com>
2026-04-27 23:07:37 +0800

    fix:bt pose bug

--- src/decision/bt_manager/include/bt_manager/action/nav_action.hpp
+++ src/decision/bt_manager/include/bt_manager/action/nav_action.hpp
@@ -82,7 +82,7 @@ public:
   {
     int current_mode = -1;
     config().blackboard->get<int>("current_mode", current_mode);
-    return current_mode == Sentry_BT::NavMode::PATROL || name() == "PublishPatrolGoal";
+    return current_mode == Sentry_BT::NavMode::PATROL;
   }
 
   void on_tick() override
