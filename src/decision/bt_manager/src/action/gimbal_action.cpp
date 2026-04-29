12127fc74b83f6ff152d46e4b0dc23e7454c0b8a
luomu <2917433538@qq.com>
2026-04-29 19:52:57 +0800

    some gimbal tree logic

--- src/decision/bt_manager/src/action/gimbal_action.cpp
+++ src/decision/bt_manager/src/action/gimbal_action.cpp
@@ -99,49 +99,82 @@ BT::NodeStatus SetGimbalPoseByAreaAction::tick()
 {
   auto blackboard = config().blackboard;
   const auto pose = blackboard->get<geometry_msgs::msg::Pose>("current_pose");
+  const auto enemy_outpost_remain = !blackboard->get<bool>("enemy_outpost_destroyed");
   TacticalMode tactical_mode = TacticalMode::BALANCED;
   blackboard->get<TacticalMode>("tactical_mode", tactical_mode);
 
   float yaw_min = -180.0f;
   float yaw_max = 180.0f;
-  float pitch = 0.0f;
+  float pitch_min = -10.0f;
+  float pitch_max = 20.0f;
 
   const Point2D p{pose.position.x, pose.position.y, 0.0};
+  auto gimbal_it = tactical_gimbal_map.find(tactical_mode);
+  // if (highland_zone.contains(p)) {
+  //   yaw_min = -50.0f;
+  //   yaw_max = 50.0f;
+  //   pitch_min = 15.0f;
+  //   pitch_max = 60.0f;
+  // } else if (own_defense_zone.contains(p)) {
+  //   yaw_min = -120.0f;
+  //   yaw_max = 20.0f;
+  //   pitch_min = 8.0f;
+  //   pitch_max = 60.0f;
+  // } else if (enemy_defense_zone.contains(p)) {
+  //   yaw_min = -20.0f;
+  //   yaw_max = 120.0f;
+  //   pitch_min = 5.0f;
+  //   pitch_max = 60.0f;
+  // } else {
+  //   auto gimbal_it = tactical_gimbal_map.find(tactical_mode);
+  //   if (gimbal_it != tactical_gimbal_map.end() && !gimbal_it->second.empty()) {
+  //     const auto & rule = gimbal_it->second.front();
+  //     yaw_min = rule.yaw_lower_bound_deg;
+  //     yaw_max = rule.yaw_upper_bound_deg;
+  //     pitch_min = rule.pitch_up ? 12.0f : 0.0f;
+  //     pitch_max = rule.pitch_up ? 60.0f : 90.0f;
+  //   }
+  // }
   if (highland_zone.contains(p)) {
-    yaw_min = -50.0f;
-    yaw_max = 50.0f;
-    pitch = 15.0f;
-  } else if (own_defense_zone.contains(p)) {
-    yaw_min = -120.0f;
-    yaw_max = 20.0f;
-    pitch = 8.0f;
-  } else if (enemy_defense_zone.contains(p)) {
-    yaw_min = -20.0f;
-    yaw_max = 120.0f;
-    pitch = 5.0f;
-  } else {
-    auto gimbal_it = tactical_gimbal_map.find(tactical_mode);
-    if (gimbal_it != tactical_gimbal_map.end() && !gimbal_it->second.empty()) {
-      const auto & rule = gimbal_it->second.front();
-      yaw_min = rule.yaw_lower_bound_deg;
-      yaw_max = rule.yaw_upper_bound_deg;
-      pitch = rule.pitch_up ? 12.0f : 0.0f;
+    if ((enemy_outpost_buff_zone.contains(p) || enemy_outpost_watch_zone.contains(p)) && enemy_outpost_remain) {
+      // 前哨站区域内优先使用敌方前哨站的巡逻规则
+      yaw_min = -50.0f;
+      yaw_max = 50.0f;
+      pitch_min = 15.0f;
+      pitch_max = 60.0f;
+    } else {
+      if (gimbal_it != tactical_gimbal_map.end() && !gimbal_it->second.empty()) {
+        const auto & rule = gimbal_it->second.front();
+        yaw_min = rule.yaw_lower_bound_deg;
+        yaw_max = rule.yaw_upper_bound_deg;
+        pitch_min = rule.pitch_up ? 12.0f : -10.0f;
+        pitch_max = rule.pitch_up ? 60.0f : 20.0f;
+      }
     }
+  } else if (own_defense_zone.contains(p)) {
+    yaw_min = -180.0f;
+    yaw_max = 180.0f;
+  } else if (enemy_defense_zone.contains(p)) {
+    yaw_min = -180.0f;
+    yaw_max = 180.0f;
   }
-
   blackboard->set<float>("scan_yaw_min_deg", yaw_min);
   blackboard->set<float>("scan_yaw_max_deg", yaw_max);
-  blackboard->set<float>("scan_pitch_deg", pitch);
+  blackboard->set<float>("scan_pitch_min_deg", pitch_min);
+  blackboard->set<float>("scan_pitch_max_deg", pitch_max);
+
 
   static float last_yaw_min = 0.0f;
   static float last_yaw_max = 0.0f;
-  static float last_pitch = 0.0f;
-  if (yaw_min != last_yaw_min || yaw_max != last_yaw_max || pitch != last_pitch) {
-    std::cout << "SetGimbalPoseByAreaAction => yaw[" << yaw_min << ", " << yaw_max << "], pitch=" << pitch
-              << std::endl;
+  static float last_pitch_min = 0.0f;
+  static float last_pitch_max = 0.0f;
+  if (yaw_min != last_yaw_min || yaw_max != last_yaw_max || pitch_min != last_pitch_min || pitch_max != last_pitch_max) {
+    std::cout << "SetGimbalPoseByAreaAction => yaw[" << yaw_min << ", " << yaw_max << "], pitch_min=" << pitch_min
+              << ", pitch_max=" << pitch_max << std::endl;
     last_yaw_min = yaw_min;
     last_yaw_max = yaw_max;
-    last_pitch = pitch;
+    last_pitch_min = pitch_min;
+    last_pitch_max = pitch_max;
   }
 
   return BT::NodeStatus::SUCCESS;
