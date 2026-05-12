8fd7eaa8b2b6929bc3563b6c32c7a780bbe42465
zhong-jia-xu <wer22293@163.com>
2026-05-12 06:36:13 +0000

    multilidar

--- src/decision/bt_manager/src/bt_manager.cpp
+++ src/decision/bt_manager/src/bt_manager.cpp
@@ -78,7 +78,6 @@ void SentryBTManager::registerNodes()
   factory_.registerNodeType<SetTunnelRecoveryAttemptPoint>("SetTunnelRecoveryAttemptPoint");
   factory_.registerNodeType<SetTunnelRecoveryRetreatPoint>("SetTunnelRecoveryRetreatPoint");
   factory_.registerNodeType<SetGlobalVelocity>("SetGlobalVelocity");
-  factory_.registerNodeType<EmergencyStop>("EmergencyStop");
 
   // stance
   factory_.registerNodeType<CheckHeat>("CheckHeat");
