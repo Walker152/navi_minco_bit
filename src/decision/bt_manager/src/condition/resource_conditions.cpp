38abc582a69b20921452d7fd6cdd9c3b6e3c0688
alioth <15207309998@163.com>
2026-05-24 00:23:14 +0800

    比赛版本

--- src/decision/bt_manager/src/condition/resource_conditions.cpp
+++ src/decision/bt_manager/src/condition/resource_conditions.cpp
@@ -243,7 +243,7 @@ BT::NodeStatus CheckAttackFortHealthExchangeNeeded::tick()
   const bool in_attack_transform_zone =
     transform_zone[1].contains(point) || transform_zone[3].contains(point);
   const bool attack_fort_window =
-    game_status == 4 && game_time_remaining >= 0 && game_time_remaining <= 120 && big_energy_status == 2;
+    game_status == 4 && game_time_remaining >= 0 && game_time_remaining <= 120 && big_energy_status == 1;
   if (!attack_fort_window) {
     requested_ = false;
   }
