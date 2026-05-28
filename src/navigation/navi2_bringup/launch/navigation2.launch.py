959ea9e112f3e4c5e09453c6133f3ef142c221a6
枫落醉红颜 <15207309998@163.com>
2026-05-28 16:53:43 +0800

    改动地图2

--- src/navigation/navi2_bringup/launch/navigation2.launch.py
+++ src/navigation/navi2_bringup/launch/navigation2.launch.py
@@ -54,20 +54,20 @@ def generate_launch_description():
             output='screen'),
 
         # 静态TF: camera_init -> body
-        # launch_ros.actions.Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='static_tf_camera_init_to_body',
-        #     arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
-        #     output='screen'),
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_camera_init_to_body',
+            arguments=['0', '0', '0', '0', '0', '0', 'camera_init', 'body'],
+            output='screen'),
 
-        # # 静态TF: body -> base_link
-        # launch_ros.actions.Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='static_tf_body_to_base_link',
-        #     arguments=['0', '0.20', '0', '0', '0', '0', 'body', 'base_link'],
-        #     output='screen'),
+        # 静态TF: body -> base_link
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_body_to_base_link',
+            arguments=['0', '0.20', '0', '0', '0', '0', 'body', 'base_link'],
+            output='screen'),
         # 声明新的 Launch 参数
         launch.actions.DeclareLaunchArgument('use_sim_time', default_value=use_sim_time,
                                              description='Use simulation (Gazebo) clock if true'),
