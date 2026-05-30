fe1a5f33f5f6fe4b7fe05c82d1bc8acfd024601f
zhong-jia-xu <wer22293@163.com>
2026-05-31 02:10:12 +0800

    1

--- start.bash
+++ start.bash
@@ -3,7 +3,7 @@ cd ~/2025-sentry-navi
 source ./install/setup.bash
 
 # PTP Sync
-gnome-terminal -- bash -c "sudo ./scripts/ptp_sync.bash; exec bash"
+# gnome-terminal -- bash -c "sudo ./scripts/ptp_sync.bash; exec bash"
 # sleep 10
 
 # MID360 
@@ -14,16 +14,16 @@ sleep 1
 # gnome-terminal -- bash -c "ros2 launch lidar_merger dual_lidar_merger.launch.py; exec bash"
 # sleep 3
 
-# # SLAM
-# gnome-terminal -- bash -c "ros2 launch point_lio point_lio.launch.py; exec bash"
-# sleep 1
+# SLAM
+gnome-terminal -- bash -c "ros2 launch point_lio point_lio.launch.py; exec bash"
+sleep 1
 
 # ICP
 # gnome-terminal -- bash -c "ros2 launch icp_relocalization gicp_relocalization.launch.py; exec bash"
 # sleep 1
 
 # Cloud Crop Filter
-# gnome-terminal -- bash -c "ros2 launch msg_convert cloud_registered_crop_filter.launch.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch msg_convert cloud_registered_crop_filter.launch.py; exec bash"
 sleep 1
 
 # Navigation2
