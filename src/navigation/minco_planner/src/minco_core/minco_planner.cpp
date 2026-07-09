f8efb2f386154182d9ad1cf5a02fc6dec7725637
枫落醉红颜 <15207309998@163.com>
2026-07-09 12:58:02 +0800

    fix:remove planner max allowed cost constraint

--- src/navigation/minco_planner/src/minco_core/minco_planner.cpp
+++ src/navigation/minco_planner/src/minco_core/minco_planner.cpp
@@ -1123,12 +1123,13 @@ bool MincoPlanner::ReplanLocal(const geometry_msgs::msg::PoseStamped & current_p
       std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now() - opt_start_steady).count();
   }
 
-  const double max_allowed_cost = 6000.0;
-  if (!std::isfinite(final_cost) || final_cost > max_allowed_cost) {
-    RCLCPP_WARN(logger_,
-      "[MincoPlanner] Rejecting new trajectory! Cost (%.2f) exceeds limit (%.2f).",
-      final_cost,
-      max_allowed_cost);
+  // const double max_allowed_cost = 6000.0;
+  // if (!std::isfinite(final_cost) || final_cost > max_allowed_cost) {
+  if (!std::isfinite(final_cost)) {
+    // RCLCPP_WARN(logger_,
+    //   "[MincoPlanner] Rejecting new trajectory! Cost (%.2f) exceeds limit (%.2f).",
+    //   final_cost,
+    //   max_allowed_cost);
 
     bool has_last_traj = false;
     {
