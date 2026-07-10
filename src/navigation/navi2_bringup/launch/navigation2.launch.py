e9abdbc024e89e359f0bac24790193461545d770
alioth <15207309998@163.com>
2026-07-10 17:18:41 +0800

    new_map

--- src/navigation/navi2_bringup/launch/navigation2.launch.py
+++ src/navigation/navi2_bringup/launch/navigation2.launch.py
@@ -18,11 +18,11 @@ def generate_launch_description():
         'use_sim_time', default='False')
     
     map_yaml_path = launch.substitutions.LaunchConfiguration(
-        'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026.yaml'))
+        # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026.yaml'))
         # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmul2026.yaml'))
         # 'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/rmuc2026_back.yaml'))
         #  'map', default=os.path.join(DreamChaser_dir, 'maps', '2026/room.yaml'))
-        # 'map', default=os.path.join(DreamChaser_dir, 'maps', 'first_floor/first_floor.yaml'))
+        'map', default=os.path.join(DreamChaser_dir, 'maps', 'first_floor/first_floor.yaml'))
     nav2_param_path = launch.substitutions.LaunchConfiguration(
         'params_file', default=os.path.join(DreamChaser_dir, 'params', 'sentry1.yaml'))
 
@@ -35,12 +35,12 @@ def generate_launch_description():
             # arguments=['1.73', '6.4', '0', '0.0', '0', '0.0', 'map', 'camera_init'],
             # arguments=['5', '7', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['1', '3', '0', '0', '0', '0', 'map', 'camera_init'],
-            # arguments=['6.7', '3.9', '0', '0', '0', '0', 'map', 'camera_init'],
+            arguments=['6.7', '4.0', '0', '0', '0', '0', 'map', 'camera_init'],
             # arguments=['9.0', '4.0', '0', '1.57', '0', '0', 'map', 'camera_init'],
             # 2026 rmuc
             # x坐标：台阶到基地边缘1655mm+基地长1881mm+车身y半长198.5mm+地图投影边缘300mm=4036mm
             # y坐标：场地y半长7500mm-半车宽191mm=7309mm
-            arguments=['4.034', '7.3', '0', '0', '0', '0', 'map', 'camera_init'],
+            # arguments=['4.034', '7.3', '0', '0', '0', '0', 'map', 'camera_init'],
             output='screen'),
 
         # 静态TF: map -> minimap
