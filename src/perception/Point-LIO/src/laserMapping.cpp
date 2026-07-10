a601d4d917ec86b070e64f1270bac8e74a07fadd
枫落醉红颜 <15207309998@163.com>
2026-07-10 16:41:26 +0800

    none

--- src/perception/Point-LIO/src/laserMapping.cpp
+++ src/perception/Point-LIO/src/laserMapping.cpp
@@ -1136,7 +1136,7 @@ void publish_odometry(const rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPt
     tf2::Quaternion q;
     q.setRPY(0, 0, yaw_pose);
     // tf2::Vector3 offset_vec(0.0, 0.15, 0.0);
-    tf2::Vector3 offset_vec(0.0, -0.20, 0.0);  // 变形哨
+    tf2::Vector3 offset_vec(0.0, 0.20, 0.0);  // 变形哨
     tf2::Vector3 base_pose =
       tf2::Vector3(odomAftMapped.pose.pose.position.x, odomAftMapped.pose.pose.position.y, odomAftMapped.pose.pose.position.z) +
       tf2::quatRotate(q, offset_vec);
