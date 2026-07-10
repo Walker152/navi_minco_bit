93c75bc8eb01f2adee2187f84a53e3a1c47d0a1f
alioth <15207309998@163.com>
2026-07-10 18:44:12 +0800

    test

--- start.bash
+++ start.bash
@@ -3,12 +3,12 @@ cd ~/2025-sentry-navi
 source ./install/setup.bash
 
 # PTP Sync
-# gnome-terminal -- bash -c "sudo ./scripts/ptp_sync.bash; exec bash"
+gnome-terminal -- bash -c "sudo ./scripts/ptp_sync.bash; exec bash"
 # sleep 8
 
 # Driver + SLAM
 # gnome-terminal -- bash -c "ros2 launch point_lio mixed_livox_pointlio_intra_process.launch.py; exec bash"
-gnome-terminal -- bash -c "ros2 launch point_lio single_livox_pointlio_intra_process.launch.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch point_lio mixed_livox_pointlio_intra_process.launch.py; exec bash"
 sleep 3
 
 # ICP
@@ -25,7 +25,7 @@ gnome-terminal -- bash -c "ros2 launch navi2 navigation2.launch.py; exec bash"
 sleep 5
 
 # Decision
-gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
+# gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
 # sleep 3
 
 # Communication
@@ -54,7 +54,7 @@ ROSBAG_TOPICS=(
   # /livox/lidar
   # /livox/imu_192_168_1_135
 )
-# gnome-terminal -- bash -c "mkdir -p ~/rosbag && ros2 bag record -o ~/rosbag/$(date +%Y%m%d_%H%M%S) ${ROSBAG_TOPICS[*]}; exec bash"
+gnome-terminal -- bash -c "mkdir -p ~/rosbag && ros2 bag record -o ~/rosbag/$(date +%Y%m%d_%H%M%S) ${ROSBAG_TOPICS[*]}; exec bash"
 
 # Foxglove
 # gnome-terminal -- bash -c "ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765; exec bash"
