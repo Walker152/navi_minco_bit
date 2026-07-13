fa418191f6782936aa37222460e48c35124d95b5
alioth <15207309998@163.com>
2026-07-13 16:25:24 +0800

    test

--- src/decision/bt_manager/include/bt_manager/utils/area.hpp
+++ src/decision/bt_manager/include/bt_manager/utils/area.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#define RMUC_AREA
-// #define TEST_AREA
+// #define RMUC_AREA
+#define TEST_AREA
 #include "bt_manager/utils/nav_zone.hpp"
 
 #include <array>
@@ -209,8 +209,8 @@ inline std::array<AreaPolygon<6, Point2D>, 4> transform_zone{
     Point2D{9.6, 7.3}, Point2D{11.1, 7.3}, Point2D{12.6, 7.3},
     Point2D{12.6, 1.5}, Point2D{11.1, 1.5}, Point2D{9.6, 1.5}}, // TODO: replace with measured 6-point polygon vertices
   AreaPolygon<6, Point2D>{
-    Point2D{6.9, 3.5}, Point2D{5.7, 2.7}, Point2D{3.3, 3.3},
-    Point2D{2.4, 3.3}, Point2D{2.4,1.4}, Point2D{6.9, 1.4}}, // TODO: replace with measured 6-point polygon vertices
+    Point2D{7.5, 3.5}, Point2D{5.7, 3.5}, Point2D{2.4, 3.5},
+    Point2D{2.4, 1.4}, Point2D{5.7,1.4}, Point2D{7.5, 1.4}}, // TODO: replace with measured 6-point polygon vertices
 };
 inline std::array<Area_Square, 2> bonus_zone = {
   Area_Square{Point2D{12.8, 5.5}, Point2D{13.8, 6.5}},
@@ -220,14 +220,14 @@ inline std::array<Area_Square, 4> tunnel_zone = {
   Area_Square{Point2D{12.6, 7.2}, Point2D{11.4, 5.3}},
   Area_Square{Point2D{12.6, 7.2}, Point2D{11.4, 5.3}},
   Area_Square{Point2D{12.6, 7.2}, Point2D{11.4, 5.3}},
-  Area_Square{Point2D{5.7, 2.6}, Point2D{4.4, 1.4}},
+  Area_Square{Point2D{5.7, 2.6}, Point2D{2.4, 1.4}},
 };
 // Per-tunnel recovery configuration, index-aligned with tunnel_zone.
 inline std::array<TunnelRecoveryConfig, 4> tunnel_recovery_configs = {
   TunnelRecoveryConfig{1.57f},
   TunnelRecoveryConfig{1.57f},
   TunnelRecoveryConfig{1.57f},
-  TunnelRecoveryConfig{0.0f},
+  TunnelRecoveryConfig{3.14f},
 };
 inline std::array<Area_Square, 2> stairs_zone{
   Area_Square{Point2D{11.5, 7.1}, Point2D{10.3, 6.2}},
@@ -348,9 +348,11 @@ using PatrolList = std::vector<PatrolPoint>;
 inline std::vector<PatrolList> normal_patrol_branches = {
   // for test
   {
-    {{10.0, 2.5, 0.0}, 5000},
-    {{12.7, 2.8, 0.0}, 5000},
-    {{9.6, 2.6, 0.0}, 5000}
+    // {{10.0, 2.5, 0.0}, 5000},
+    // {{12.7, 2.8, 0.0}, 5000},
+    // {{9.6, 2.6, 0.0}, 5000},
+    {{3.9, 4.9, 0.0}, 5000},
+    {{8.0, 2.7, 0.0}, 5000}
   }
 };
 
@@ -358,7 +360,6 @@ inline std::vector<PatrolList> attack_patrol_branches = {
   {
     {{16.0, 12.0, 0.0}, 5000},
     {{17.3, 7.9, 0.0}, 5000},
-
     {{15.3, 11.0, 0.0}, 6000}
   }
 };
