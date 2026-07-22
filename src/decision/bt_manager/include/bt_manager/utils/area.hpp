ae980770b8d2f149db621d11c169b8310a15649b
alioth <15207309998@163.com>
2026-07-22 18:50:56 +0800

    for test

--- src/decision/bt_manager/include/bt_manager/utils/area.hpp
+++ src/decision/bt_manager/include/bt_manager/utils/area.hpp
@@ -209,8 +209,8 @@ inline std::array<AreaPolygon<6, Point2D>, 4> transform_zone{
     Point2D{10.6, 7.3}, Point2D{11.1, 7.3}, Point2D{12.6, 7.3},
     Point2D{12.6, 3.0}, Point2D{11.1, 3.0}, Point2D{10.6, 3.0}}, // TODO: replace with measured 6-point polygon vertices
   AreaPolygon<6, Point2D>{
-    Point2D{8.0, 4.0}, Point2D{5.7, 4.0}, Point2D{2.4, 4.0},
-    Point2D{2.4, 1.4}, Point2D{5.7,1.4}, Point2D{8.0, 1.4}}, // TODO: replace with measured 6-point polygon vertices
+    Point2D{8.5, 4.0}, Point2D{5.7, 4.0}, Point2D{2.4, 4.0},
+    Point2D{2.4, 1.4}, Point2D{5.7,1.4}, Point2D{8.5, 1.4}}, // TODO: replace with measured 6-point polygon vertices
 };
 inline std::array<Area_Square, 2> bonus_zone = {
   Area_Square{Point2D{12.8, 5.5}, Point2D{13.8, 6.5}},
@@ -352,7 +352,7 @@ inline std::vector<PatrolList> normal_patrol_branches = {
     {{11.0, 3.1, 0.0}, 5000},
     {{15.0, 4.2, 0.0}, 5000},
     {{3.9, 4.9, 0.0}, 10000},
-    // {{10.0, 2.7, 0.0}, 10000}
+    {{8.1, 6.4, 0.0}, 10000}
   },
   {
     {{3.9, 4.9, 0.0}, 10000},
