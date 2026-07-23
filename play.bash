a419a66d2743026a618ad060b2ed49e4581c7b4d
alioth <15207309998@163.com>
2026-07-23 22:10:16 +0800

    test

--- play.bash
+++ play.bash
@@ -1,16 +1,4 @@
-
-#param_client_test
-# source ~/ws01_plumbing_ljy/install/setup.bash
-# gnome-terminal -- bash -c "ros2 run param study02_param_client ; exec bash"
-# sleep 2s
 # Navigation2
 source ./install/setup.bash
 gnome-terminal -- bash -c "ros2 launch navi2 navigation2.launch.py; exec bash"
 sleep 0.5s
-# BT-manager
-# gnome-terminal -- bash -c "ros2 launch bt_manager bt_manager.launch.py; exec bash"
-sleep 0.5s
-# commmunication
-# gnome-terminal -- bash -c "ros2 launch communication com.launch.py; exec bash"
-sleep 0.5s
-
