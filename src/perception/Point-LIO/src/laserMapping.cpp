d71f9838a0ad5398f0d464a342fc3d638a9edc65
luomu <2917433538@qq.com>
2026-07-31 19:09:42 +0800

    Revert "save pcd"
    This reverts commit 59b84e48efd6d0dd7a2f3d68c94b579245f5432d.

--- src/perception/Point-LIO/src/laserMapping.cpp
+++ src/perception/Point-LIO/src/laserMapping.cpp
@@ -42,8 +42,7 @@ string root_dir = ROOT_DIR;
 
 bool init_map = false, flg_first_scan = true;
 
-bool flg_reset = false;
-std::atomic_bool flg_exit{false};
+bool flg_reset = false, flg_exit = false;
 
 // surf feature in map
 PointCloudXYZI::Ptr feats_undistort(new PointCloudXYZI());
@@ -406,18 +405,6 @@ void publish_accumulated_map(
 
 PointCloudXYZI::Ptr pcl_wait_save(new PointCloudXYZI());
 
-void save_pending_pcd()
-{
-  if (!pcl_wait_save || pcl_wait_save->empty() || !pcd_save_en) {
-    return;
-  }
-
-  const string all_points_dir(string(ROOT_DIR) + "PCD/scans.pcd");
-  pcl::PCDWriter pcd_writer;
-  pcd_writer.writeBinary(all_points_dir, *pcl_wait_save);
-  pcl_wait_save->clear();
-}
-
 point_lio::StateLogRecord makeStateLogRecord()
 {
   point_lio::StateLogRecord record;
@@ -744,7 +731,12 @@ public:
   ~LaserMappingNode() override
   {
     stopWorker();
-    save_pending_pcd();
+    if (!pcl_wait_save->empty() && pcd_save_en) {
+      string file_name = string("scans.pcd");
+      string all_points_dir(string(string(ROOT_DIR) + "PCD/") + file_name);
+      pcl::PCDWriter pcd_writer;
+      pcd_writer.writeBinary(all_points_dir, *pcl_wait_save);
+    }
     RuntimeStatistics::instance().shutdown();
   }
 
@@ -1639,7 +1631,6 @@ void LaserMappingNode::processingLoop()
     }
     rate.sleep();
   }
-  save_pending_pcd();
 }
 
 }  // namespace point_lio
@@ -1658,10 +1649,7 @@ int main(int argc, char ** argv)
   auto node = std::make_shared<point_lio::LaserMappingNode>(options);
   rclcpp::executors::MultiThreadedExecutor executor;
   executor.add_node(node);
-  // Let the signal handler stop the worker first so the node destructor can flush scans.pcd.
-  while (rclcpp::ok() && !flg_exit.load()) {
-    executor.spin_once(std::chrono::milliseconds(100));
-  }
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
