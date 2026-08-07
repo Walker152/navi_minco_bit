b21ece17346904b08ba8d0948d1e49326150771d
alioth <15207309998@163.com>
2026-08-08 00:43:08 +0800

    reset behavior 8.6

--- src/decision/bt_manager/src/main.cpp
+++ src/decision/bt_manager/src/main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char const * argv[])
   auto blackboard = std::make_shared<Sentry_BT::Blackboard>();
   auto ros_interface_node = std::make_shared<Sentry_BT::ros_interface>(blackboard);
   auto transform_utils_node = std::make_shared<Sentry_BT::TransformUtils>();
-
   const bool bt_debug_logs = ros_interface_node->declare_parameter<bool>("bt_debug_logs", true);
   const bool bt_debug_log_to_file =
     ros_interface_node->declare_parameter<bool>("bt_debug_log_to_file", false);
